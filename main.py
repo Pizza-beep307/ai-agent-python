d8d766145bebeff894305225fab78ba6467a3700
e2501754 <chelda.e2501754@etud.univ-ubs.fr>
2026-04-25 00:24:47 +0200

    Adding input user for the prompt

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
     print("Hello from ai-agent-project!")
+
+    load_dotenv()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    question = input("prompt : ")
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=question
+    )
+    print(response.text)
+
 
 
 if __name__ == "__main__":
